2588b5d25a54e8afc709c1764edea01cfc817ef9
Thalles-PeSilva <thallespe.silva@gmail.com>
2023-06-04 18:33:27 -0300

    firt commit

---
+++ app/src/main/java/Model/Task.java
@@ -0,0 +1,129 @@
+
+package Model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author thall
+ */
+public class Task {
+    
+    private int id;
+    private int idProject;
+    private String name;
+    private String description;
+    private String notes;
+    private boolean isCompledted;
+    private Date deadline;
+    private Date createdAt;
+    private Date updatedAt;
+
+    public Task(int id, int idProject, String name, String description, String notes, boolean isCompledted, Date deadline, Date createdAt, Date updatedAt) {
+        this.id = id;
+        this.idProject = idProject;
+        this.name = name;
+        this.description = description;
+        this.notes = notes;
+        this.isCompledted = isCompledted;
+        this.deadline = deadline;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+    
+    public Task(){
+        this.createdAt = new Date();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getIdProject() {
+        return idProject;
+    }
+
+    public void setIdProject(int idProject) {
+        this.idProject = idProject;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getNots() {
+        return notes;
+    }
+
+    public void setNots(String nots) {
+        this.notes = nots;
+    }
+
+    public boolean isIsCompledted() {
+        return isCompledted;
+    }
+
+    public void setIsCompledted(boolean isCompledted) {
+        this.isCompledted = isCompledted;
+    }
+
+    public Date getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(Date deadline) {
+        this.deadline = deadline;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" + "id=" + id + ", idProject=" + idProject + ", name=" + name + ", description=" + description + ", nots=" + notes + ", isCompledted=" + isCompledted + ", deadline=" + deadline + ", createdAt=" + createdAt + ", updatedAt=" + updatedAt + '}';
+    }
+
+    public String getNotes() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public void setNotes(String string) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public void setIsCompleted(boolean aBoolean) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+    
+    
+}
